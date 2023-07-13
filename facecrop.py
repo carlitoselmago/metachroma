a585903c153aaf4bbefd74e07e4cbfa00cd1d4f7
carlitoselmago <ccarbonell@gmail.com>
2023-07-13 19:27:20 +0200

    fixed warping

--- facecrop.py
+++ facecrop.py
@@ -1,7 +1,5 @@
 import cv2
 import mediapipe as mp
-import numpy as np
-from PIL import Image
 
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
@@ -13,20 +11,23 @@ face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False,
 
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-cap = cv2.VideoCapture(2)
+#cv2.namedWindow("img", cv2.WND_PROP_FULLSCREEN)
+#cv2.namedWindow("img", cv2.WINDOW_FREERATIO)
+#cv2.setWindowProperty("img", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+cap = cv2.VideoCapture(1)
 width = 1920
 height = 1080
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-zoom_scale = 2  # decrease this value to decrease the margin
-transition_frames = 30  # You can modify transition_frames as needed
 
-bbox_curr = [0, 0, cap.get(cv2.CAP_PROP_FRAME_WIDTH),
-             cap.get(cv2.CAP_PROP_FRAME_HEIGHT)]   # Initial bounding box
+zoom_scale = 5  # decrease this value to decrease the margin
+transition_frames = 30  # You can modify `transition_frames` as needed
+
+bbox_curr = [0, 0, cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)]   # Initial bounding box
 frame_count = 0
 
 while True:
     success, img = cap.read()
+
     if not success:
         break
 
@@ -40,8 +41,8 @@ while True:
             x_values, y_values = [], []
 
             for i in eye_points:
-                x = int(face_landmarks.landmark[i].x * img.shape[1]) 
-                y = int(face_landmarks.landmark[i].y * img.shape[0])
+                x = int(face_landmarks.landmark[i].x * img_rgb.shape[1]) 
+                y = int(face_landmarks.landmark[i].y * img_rgb.shape[0])
                 x_values.append(x)
                 y_values.append(y)
 
@@ -57,7 +58,7 @@ while True:
     else:
         bx = 0
         by = 0
-        b_dim = max(img.shape[1], img.shape[0])
+        b_dim = max(img_rgb.shape[1], img_rgb.shape[0])
 
     if transition_frames > 0:
         frame_count = (frame_count + 1) % transition_frames
@@ -67,10 +68,33 @@ while True:
 
     bbox_curr = [bx, by, b_dim]
 
-    #cv2.rectangle(img, (bx, by), (bx+b_dim, by+b_dim), (0,255,0), 2)
-    #cv2.imshow('Bounding Box', img)
-    img_cropped = img[by:by+b_dim, bx:bx+b_dim]
-    img_resized = cv2.resize(img_cropped, (1920, 1080))
+    img_cropped = img_rgb[by:by+b_dim, bx:bx+b_dim]
+    img_resized = cv2.resize(img_cropped, (width, height))
+    img_resized = cv2.cvtColor(img_resized, cv2.COLOR_RGB2BGR)  # Convert from RGB to BGR
+
+    ###
+    # aspect ratio of original image
+    aspratio_crop = img_cropped.shape[1] / img_cropped.shape[0]
+    # aspect ratio of desired size
+    aspratio_screen = width / height
+
+    if(aspratio_crop < aspratio_screen):
+        # resize width to fit
+        wtemp = width
+        htemp = int(width / aspratio_crop)
+    else:
+        htemp = height
+        wtemp = int(height * aspratio_crop)
+
+    img_temp = cv2.resize(img_cropped, (wtemp, htemp))
+
+    # Cropping the image
+    startRow = int(max(0,int((img_temp.shape[0]-height)/2)))
+    startCol = int(max(0,int((img_temp.shape[1]-width)/2)))
+    img_resized = img_temp[startRow:(startRow+height), startCol:(startCol+width)]
+
+    img_resized = cv2.cvtColor(img_resized, cv2.COLOR_RGB2BGR)  # Convert from RGB to BGR
+    ###
     cv2.imshow("Image", img_resized)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
